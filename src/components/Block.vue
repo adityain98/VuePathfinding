7908dca723a27da40090f9136517d03b2f7d952c
Indra Aditya <indra.aditya@sekolahmu.co.id>
2020-06-07 02:55:51 +0700

    first commit

---
+++ src/components/Block.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="block" :class="{'wall': item.isWall}" @mousedown.left="onMouseClick()" @mouseover="onMouseOver()">
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    onMouseOver() {
+      this.$emit('onMouseOver', this.index)
+    },
+    onMouseClick() {
+      this.$emit('onMouseClick', this.index)
+    }
+  },
+  props: {
+    item: Object,
+    index: Number,
+    isClick: Boolean
+  }
+}
+</script>
+
+<style scoped>
+
+.block {
+  background: white;
+  border: 1px solid grey;
+}
+
+.wall {
+  background-color: #0C3546;
+  border: none;
+  animation-name: pulse;
+  animation-duration: 0.3s;
+}
+
+@keyframes pulse {
+	0% {
+		transform: scale(1);
+    background-color: #1a6f91
+	}
+
+  50% {
+    transform: scale(1.2);
+    background-color: #13526b
+  }
+
+	100% {
+		transform: scale(1);
+    background-color: #0C3546;
+	}
+}
+
+</style>
